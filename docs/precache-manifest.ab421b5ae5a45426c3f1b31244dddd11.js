aa1e9fe4926270a151a27427771fb7006d0dae17
Marcelino Ponce <marceponcef@gmail.com>
2020-09-17 20:10:07 +0200

    docs agregado

---
+++ docs/precache-manifest.ab421b5ae5a45426c3f1b31244dddd11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99eeff157be9fdc8d9f7a66d521c59a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b000e01c46b88a4f0683",
+    "url": "/static/css/main.c4b39337.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b000e01c46b88a4f0683",
+    "url": "/static/js/main.543e31f9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
